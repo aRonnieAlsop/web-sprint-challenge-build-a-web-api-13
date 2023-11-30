d0bb588056a86d30a0471457414d1e762558ad82
Arron Alsop <alsop.arron@gmail.com>
2023-11-29 18:42:08 -0800

    'wip'

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,8 +1,25 @@
 // Write your "projects" router here!
 const express = require('express')
-
+const Project = require('./projects-model')
 
 const router = express.Router()
+
+// - [ ] `[GET] /api/projects`
+//   - Returns an array of projects as the body of the response.
+//   - If there are no projects it responds with an empty array.
+router.get('/', (req, res) => {
+    Project.get()
+        .then(projects => {
+            if (projects) {
+                res.status(200).json(projects)
+            } else {
+                res.status(200).json([])
+            }
+        }) 
+        .catch(err => {
+            res.status(500).json({ message: `oops` })
+        }) 
+})
 
 
 module.exports = router
